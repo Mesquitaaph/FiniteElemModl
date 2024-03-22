a135cde1a775f344bfb4bb020fd7207a3cff7f39
unknown <raphafm.rf@gmail.com>
2024-03-22 15:31:03 -0300

    unidimensional funcionando

--- main.jl
+++ main.jl
@@ -1,29 +1,103 @@
-using GaussQuadrature
+using GaussQuadrature, SparseArrays
 
-run(`clear`)
+run(`cmd /c cls`)
 
-# x, w = legendre(5)
+function montaLG(ne)
+    LG1 = [1:1:ne;]
+    LG = Matrix{Int64}(undef, 2, ne)
+    LG[1,:] = LG1[:]
+    LG[2,:] = (LG1.+1)[:]
+
+    return LG
+end
+
+function montaEQ(ne, neq)
+    EQ = zeros(Int64, ne+1, 1)
+    EQ[2:length(EQ)-1] = [1:1:neq;]
+
+    return EQ
+end
+
 
 function PHI(P)
-    return [(1-P)/2 (1+P)/2]
+    return [(1-P)/2, (1+P)/2]
 end
 
 function dPHI(P)
-    return [-1/2*(P^0) 1/2*(P^0)]
+    return [-1/2*(P^0); 1/2*(P^0)]
 end
 
-function montaK(ne, dx, alpha, beta)
+function montaK(ne, neq, dx, alpha, beta, EQoLG)
+    K = spzeros(neq, neq)
     npg = 2; P, W = legendre(npg)
 
-    phiP = PHI.(P); dphiP = dPHI.(P)
-    println(dphiP)
-    # print(dphiP)
+    phiP = hcat(PHI.(P)...); dphiP = hcat(dPHI.(P)...)
+
     Ke = 2*alpha/dx * (W.*dphiP) * dphiP' + beta*dx/2 * (W.*phiP) * phiP'
 
-    return Ke
+    for e = 1:ne
+        for a = 1:2
+            i = EQoLG[a,e]
+            for b = 1:2
+                j = EQoLG[b,e]
+                if i != 0 && j != 0
+                    K[i,j] += Ke[a,b]
+                end
+            end
+        end
+    end
+
+    return K
 end
 
-alpha = 1; beta = 1; ne = 1; dx = 1/4
-montaK(ne, dx, alpha, beta)
+function montaxPTne(dx, X)
+    return f(P) = (dx/2)*(P+1) .+ X
+end
 
-# tst = [1 -1]
+function montaF(ne, neq, X, f, EQoLG)
+    npg = 5; P, W = legendre(npg)
+
+    phiP = hcat(PHI.(P)...); dphiP = hcat(dPHI.(P)...)
+    
+    dx = 1/ne
+    xPTne = hcat(montaxPTne(dx, X[1:end-1]).(P)...)'
+
+    Fe = (dx/2*W'.*phiP) * f(xPTne)
+
+    F = zeros(neq, 1)
+    for e = 1:ne
+        for a = 1:2
+            i = EQoLG[a,e]
+            if i != 0
+                F[i] += Fe[a,e]
+            end
+        end
+    end
+
+    return F
+end
+
+function solve()
+    alpha = 1; beta = 1; ne = 2^16+1; dx = 1/ne; neq = ne-1
+    a = 0; b = 1
+    f(x) = x; u(x) = x + (ℯ^(-x) - ℯ^x)/(ℯ - ℯ^(-1))
+
+    X = [a:dx:b;]
+
+    EQ = montaEQ(ne, neq)
+    LG = montaLG(ne)
+    
+    EQoLG = EQ[LG]
+    
+    K = montaK(ne, neq, dx, alpha, beta, EQoLG)
+
+    F = montaF(ne, neq, X, f, EQoLG)'
+
+    C = F/K
+
+    return C
+end
+
+@time begin
+    solve()
+end
