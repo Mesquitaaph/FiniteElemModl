c5938dd22f96b74831d4ce0f092c3264827fddda
mesquitaaph <raphafm.rf@gmail.com>
2024-04-04 11:43:18 -0300

    comecei a otimizar in the julia way

--- v3.jl
+++ v3.jl
@@ -1,17 +1,20 @@
 using GaussQuadrature, SparseArrays, StatsBase, BenchmarkTools
 
 function montaLG(ne)
-    LG1 = [1:1:ne;]
-    LG = Matrix{Int64}(undef, 2, ne)
-    LG[1,:] = LG1[:]
-    LG[2,:] = (LG1.+1)[:]
+    LG1 = 1:1:ne
 
-    return LG
+    # return [LG1'; (LG1.+1)']
+    LG = Matrix{Int64}(undef, ne, 2)
+
+    LG[:,1] .= LG1
+    LG[:,2] .= LG1.+1
+
+    return LG'
 end
 
 function montaEQ(ne, neq)
     EQ = zeros(Int64, ne+1, 1) .+ ne
-    EQ[2:length(EQ)-1] = [1:1:neq;]
+    EQ[2:ne] .= 1:1:neq
 
     return EQ
 end
@@ -29,11 +32,11 @@ function montaK(ne, neq, dx, alpha, beta, EQoLG)
     
     phiP = reduce(vcat, PHI(P)'); dphiP = hcat(dPHI.(P)...)
     
-    Ke = 2*alpha/dx * (W.*dphiP) * dphiP' + beta*dx/2 * (W.*phiP) * phiP'
-    # println("computou Ke")
-    I = vec(EQoLG[repeat([1,2], inner=2), [1:1:ne;]])
-    J = vec(EQoLG[repeat([1,2], inner=1), repeat([1:1:ne;], inner=2)])
-    # println("computou I,J")
+    Ke = 2*alpha/dx .* (W.*dphiP) * dphiP' + beta*dx/2 .* (W.*phiP) * phiP'
+    
+    I = vec(EQoLG[repeat([1,2], inner=2), 1:1:ne])
+    J = vec(EQoLG[repeat([1,2], inner=1), repeat(1:1:ne, inner=2)])
+
     
     S = repeat(reshape(Ke, 4), outer=ne)
     
@@ -80,52 +83,53 @@ end
 function solve(alpha, beta, ne, a, b, f, u)
     dx = 1/ne; neq = ne-1
 
-    X = [a:dx:b;]
-
+    X = a:dx:b
+    
     EQ = montaEQ(ne, neq)
     LG = montaLG(ne)
     EQoLG = EQ[LG]
 
     K = montaK(ne, neq, dx, alpha, beta, EQoLG)
-    # println("K")
     
     F, xPTne = montaF(ne, neq, X, f, EQoLG)'
-    # println("F ", F)
-    # println("xPTne ", xPTne)
     
-    # println("Resolvendo sistema")
     C = F/K
-    # println("sistema")
 
-    return nothing#C, X, xPTne, EQoLG
+    return nothing#C#, X, xPTne, EQoLG
 end
-
-function convergence_test(errsize)
-    alpha = 1; beta = 1; a = 0; b = 1;
-    f(x) = x; u(x) = x + (ℯ^(-x) - ℯ^x)/(ℯ - ℯ^(-1))
-
-    NE = 2 .^ [2:1:errsize;]
-    H = 1 ./ NE
-    
-    E = zeros(length(NE))
-
-    @time begin
-        for i = 1:lastindex(NE)
-            println("Iniciando i = ", i)
-            Ci, Xi, xPTnei, EQoLGi = solve(alpha, beta, NE[i], a, b, f, u)
-            E[i] = erroVet(NE[i], xPTnei, EQoLGi, Ci, u)
-        end
-    end
-
-    return [E, H]
-end
-
-
-# E, H = convergence_test(23)
-# size(E)
-# plot!(H, E, xaxis=:log2, yaxis=:log2)
-# plot!(H, H .^2, xaxis=:log2, yaxis=:log2)
 
 alpha = 1; beta = 1; a = 0; b = 1; ne = 2^15
 f(x) = x; u(x) = x + (ℯ^(-x) - ℯ^x)/(ℯ - ℯ^(-1))
 @btime solve(alpha, beta, ne, a, b, f, u)
+
+# function convergence_test(errsize)
+#     alpha = 1; beta = 1; a = 0; b = 1;
+#     f(x) = x; u(x) = x + (ℯ^(-x) - ℯ^x)/(ℯ - ℯ^(-1))
+
+#     NE = 2 .^ [2:1:errsize;]
+#     H = 1 ./ NE
+    
+#     E = zeros(length(NE))
+
+#     @time begin
+#         for i = 1:lastindex(NE)
+#             println("Iniciando i = ", i)
+#             Ci, Xi, xPTnei, EQoLGi = solve(alpha, beta, NE[i], a, b, f, u)
+#             E[i] = erroVet(NE[i], xPTnei, EQoLGi, Ci, u)
+#         end
+#     end
+
+#     return [E, H]
+# end
+dx = 1/ne; neq = ne - 1
+EQ = montaEQ(ne, neq)
+LG = montaLG(ne)
+EQoLG = EQ[LG]
+
+function teste(alpha, beta, ne, a, b, f, u)
+    K = montaK(ne, neq, dx, alpha, beta, EQoLG)
+end
+
+# @btime teste(alpha, beta, ne, a, b, f, u)
+
+# @btime montaLG(ne)
